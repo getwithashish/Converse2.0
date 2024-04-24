7b3d86a67c171b0cc18346755c041e5c309f2f99
getwithashish <ashishsamtgeorge@mca.ajce.in>
2024-04-24 20:58:36 +0530

    fix: Resolve bug

--- converse_backend/controllers/chat_with_db.py
+++ converse_backend/controllers/chat_with_db.py
@@ -1,7 +1,10 @@
 from flask import jsonify
 from flask_jwt_extended import jwt_required
 
+from services.database_sql_talk_gemini import generate_gemini_response
+
 
 @jwt_required()
 def chatWithDb():
-    return jsonify({"ai_response": "Not Implemented Yet"}), 200
+    response = generate_gemini_response("How many car models are there?")
+    return jsonify({"ai_response": response}), 200
