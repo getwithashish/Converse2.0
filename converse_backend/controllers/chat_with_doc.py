c0dadbb77b6fb521d4df4e6f047feae66855312a
getwithashish <ashishsamtgeorge@mca.ajce.in>
2024-04-25 07:29:21 +0530

    refactor: Remove unwanted comments

--- converse_backend/controllers/chat_with_doc.py
+++ converse_backend/controllers/chat_with_doc.py
@@ -19,27 +19,17 @@ def chatWithDoc():
     else:
         response = "Provide Valid Input Message"
         status = 400
-    return jsonify({"ai_response": response}), status
+    return jsonify({"ai_response": response.get("output_text")}), status
 
 
 @jwt_required()
 def uploadDoc():
     file = request.files["file"]
-    # uploaded_file = file.read()
-    # pdf_file = io.BytesIO(uploaded_file)
-    # # print(file)
-    # create_faiss_index(pdf_file)
 
     pdf_data = file.read()
     pdf_stream = io.BytesIO(pdf_data)  # Create a stream to read the PDF data
     pdf_stream_list = [pdf_stream]
-    # pdf_reader = PdfReader(pdf_stream)
 
     create_faiss_index(pdf_stream_list)
-
-    # Extract text from the first page
-    # first_page = pdf_reader.pages[0]
-    # text = first_page.extract_text()
-    # print(text)
 
     return jsonify({"message": "Index created successfully"}), 200
